25eada6406277c5c5c1a4e475476ee01f503231f
rizwanbutt314 <rizwanbutt314@gmail.com>
2019-09-27 20:01:51 +0500

    minor changes in pbm-5

--- pbm_api/pbm_bonds/views.py
+++ pbm_api/pbm_bonds/views.py
@@ -4,8 +4,8 @@ from rest_framework import mixins, generics, filters, status
 
 
 class BondCategoryAPIIndex(mixins.ListModelMixin,
-               mixins.CreateModelMixin,
-               generics.GenericAPIView):
+                           mixins.CreateModelMixin,
+                           generics.GenericAPIView):
     serializer_class = BondCategorySerializer
     filter_backends = (filters.SearchFilter, filters.OrderingFilter)
     search_fields = ('category',)
@@ -18,14 +18,18 @@ class BondCategoryAPIIndex(mixins.ListModelMixin,
 
 
 class BondDrawDatesAPIIndex(mixins.ListModelMixin,
-               mixins.CreateModelMixin,
-               generics.GenericAPIView):
+                            mixins.CreateModelMixin,
+                            generics.GenericAPIView):
     serializer_class = BondDrawDatesSerializer
     filter_backends = (filters.SearchFilter, filters.OrderingFilter)
     search_fields = ('year', 'date',)
 
     def get_queryset(self):
-        return BondCategory.objects.get(category='7500').bonddrawdates_set
+        category = self.kwargs.get('category')
+        try:
+            return BondCategory.objects.get(category=category).bonddrawdates_set
+        except BondCategory.DoesNotExist:
+            return BondCategory.objects.none()
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
